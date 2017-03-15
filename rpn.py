56529b63789aeb47775d9c685d55545b62e99a64
John Murphy <johnkmur@umich.edu>
2017-03-15 13:35:54 -0400

    finished coloring and cleaned up

--- rpn.py
+++ rpn.py
@@ -5,16 +5,6 @@ import readline
 from colored import fg, bg, attr
 from termcolor import colored
 
-# class bcolors:
-#     HEADER = '\033[95m'
-#     OKBLUE = '\033[94m'
-#     OKGREEN = '\033[92m'
-#     WARNING = '\033[93m'
-#     FAIL = '\033[91m'
-#     ENDC = '\033[0m'
-#     BOLD = '\033[1m'
-#     UNDERLINE = '\033[4m'
-
 OPERATORS = {
 	'+': operator.add,
 	'-': operator.sub,
@@ -42,15 +32,11 @@ def main():
 	while True:
 		result = calculate(input('rpn calc> '))
 		if (result < 0):
-			#print('%s Result (is negative): %s' % (fg(1), attr(0)))
 			print(colored("Result (is negative):", 'red'), colored(result, 'red'))
 		elif (result == 0):
 			print(colored("Result:", 'yellow'), colored(result, 'yellow'))
 		else:
 			print(colored("Result (is positive):", 'green'), colored(result, 'green'))
-		# print ("Result: ", result)
-		# print ('%s Hello World !!! %s' % (fg(1), attr(0)))
-		#print(bcolors.WARNING + "Warning: No active frommets remain. Continue?" + bcolors.ENDC)
 
 if __name__ == '__main__':
 	main()
