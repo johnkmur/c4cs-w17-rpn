87896034e8d3cfb9edd8bd99cd7db281c7ab7979
John Murphy <johnkmur@umich.edu>
2017-03-15 23:52:19 -0400

    fds

--- rpn.py
+++ rpn.py
@@ -2,8 +2,9 @@
 
 import operator
 import readline
-from colored import fg, bg, attr
 from termcolor import colored
+import sys
+import os
 
 OPERATORS = {
 	'+': operator.add,
@@ -19,14 +20,44 @@ def calculate(arg):
 		try:
 			operand = float(operand)
 			stack.append(operand)
+		# except (RuntimeError, TypeError, NameError, ValueError):
+		# 	print("Error, restarting program")
+		# 	os.execv(sys.executable, ['python'] + sys.argv)
+		except RuntimeError:
+			print("RuntimeError, restarting program")
+			os.execv(sys.executable, ['python'] + sys.argv)
+
+		except TypeError:
+			print("TypeError, restarting program")
+			os.execv(sys.executable, ['python'] + sys.argv)
+
+		except NameError:
+			print("NameError, restarting program")
+			os.execv(sys.executable, ['python'] + sys.argv)
+
+		except ValueError:
+			print("ValueError, restarting program")
+			os.execv(sys.executable, ['python'] + sys.argv)
+
+		except IndexError:
+			print("ValueError, restarting program")
+			os.execv(sys.executable, ['python'] + sys.argv)
+
 		except:
-			arg2 = stack.pop()
-			arg1 = stack.pop()
-			operator_fn = OPERATORS[operand]
-			result = operator_fn(arg1, arg2)
-			
-			stack.append(result)
-	return stack.pop()
+				arg2 = stack.pop()
+				arg1 = stack.pop()
+				operator_fn = OPERATORS[operand]
+				result = operator_fn(arg1, arg2)
+				
+				stack.append(result)
+
+	try:
+		top = stack.pop()
+	except IndexError:
+		print("IndexError, restarting program")
+		os.execv(sys.executable, ['python'] + sys.argv)
+	except:
+		return top
 
 def main():
 	while True:
